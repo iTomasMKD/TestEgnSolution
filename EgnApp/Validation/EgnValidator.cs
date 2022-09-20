ae71861a77178b35ae5ae6711da76fa3f234baa4
Ilijevski7 <tomche.kubikos@hotmail.com>
2022-09-20 21:06:13 +0200

    add new things

---
+++ EgnApp/Validation/EgnValidator.cs
@@ -0,0 +1,16 @@
+﻿using EgnApp.Models;
+using FluentValidation;
+
+namespace EgnApp.Validation
+{
+    public class EgnValidator : AbstractValidator<Egn>
+    {
+        public EgnValidator()
+        {
+            RuleFor(guid => guid.Id).NotEmpty();
+            RuleFor(name => name.Name).NotEmpty();
+            RuleFor(age => age.Age).NotEmpty();
+            RuleFor(egn => egn.EgnNumber).Length(10).NotEmpty();
+        }
+    }
+}
---
+++ TestEgnApp/Validation/EgnNumberValidatorTest.cs
@@ -0,0 +1,45 @@
+﻿using EgnApp.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEgnApp.Validation
+{
+    public class EgnNumberValidatorTest
+    {
+        private readonly EgnNumberValidation _validation;
+
+        public EgnNumberValidationTests() => _validation = new EgnNumberValidation();
+
+        [Fact]
+        public void IsValid_ValidEgnNumber_ReturnsTrue()
+            => Assert.True(_validation.IsValid("1234543234"));
+
+        [Theory]
+        [InlineData("1234-3454565676-23")]
+        [InlineData("12-3454565676-23")]
+        public void IsValid_EgnNumberFirstPartWrong_ReturnsFalse(string egnNumber)
+            => Assert.False(_validation.IsValid(egnNumber));
+
+        [Theory]
+        [InlineData("12334545656")]
+        [InlineData("123-345456567633-23")]
+        public void IsValid_EgnNumberMiddlePartWrong_ReturnsFalse(string egnNumber)
+            => Assert.False(_validation.IsValid(egnNumber));
+
+        [Theory]
+        [InlineData("123-3434545656-2")]
+        [InlineData("123-3454565676-233")]
+        public void IsValid_EgnNumberLastPartWrong_ReturnsFalse(string accNumber)
+            => Assert.False(_validation.IsValid(accNumber));
+
+        [Theory]
+        [InlineData("123-345456567633=23")]
+        [InlineData("123+345456567633-23")]
+        [InlineData("123+345456567633=23")]
+        public void IsValid_InvalidDelimiters_ThrowsArgumentException(string egnNumber)
+            => Assert.Throws<ArgumentException>(() => _validation.IsValid(egnNumber));
+    }
+}
