a06d67406fd44f791d3109b3c1bc0fd32ade6b72
Ilijevski7 <tomche.kubikos@hotmail.com>
2022-09-20 20:49:38 +0200

    Add validation and repository

---
+++ EgnApp/Repository/EgnRepository.cs
@@ -0,0 +1,24 @@
+﻿using EgnApp.Contract;
+using EgnApp.Models;
+
+namespace EgnApp.Repository
+{
+    public class EgnRepository : IEgnRepository
+    {
+        private readonly EgnContext _context;
+        public EgnRepository(EgnContext context)
+        {
+            _context = context;
+        }
+        public void CreateEgn(Egn egn)
+        {
+            _context.Add(egn);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Egn> GetAll() => _context.Egn.ToList();
+
+        public Egn GetEgn(Guid id) => _context.Egn
+            .SingleOrDefault(e => e.Id.Equals(id));
+    }
+}
---
+++ EgnApp/Validation/EgnNumberValidation.cs
@@ -0,0 +1,33 @@
+﻿namespace EgnApp.Validation
+{
+    public class EgnNumberValidation
+    {
+        private const int startingPartLength = 6;
+        private const int middlePartLength = 3;
+        private const int lastPartLength = 1;
+
+        public bool IsValid(string egnNumber)
+        {
+            var firstDelimiter = egnNumber.IndexOf('-');
+            var secondDelimiter = egnNumber.LastIndexOf('-');
+
+            if (firstDelimiter == -1 || (firstDelimiter == secondDelimiter))
+                throw new ArgumentException();
+
+            var firstPart = egnNumber.Substring(0, firstDelimiter);
+            if (firstPart.Length != startingPartLength)
+                return false;
+
+            var tempPart = egnNumber.Remove(0, startingPartLength + 1);
+            var middlePart = tempPart.Substring(0, tempPart.IndexOf('-'));
+            if (middlePart.Length != middlePartLength)
+                return false;
+
+            var lastPart = egnNumber.Substring(secondDelimiter + 1);
+            if (lastPart.Length != lastPartLength)
+                return false;
+
+            return true;
+        }
+    }
+}
