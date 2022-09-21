3ef67fc527e4cb8a19afd9c98b6ad6df48abc3ab
Ilijevski7 <tomche.kubikos@hotmail.com>
2022-09-21 19:32:28 +0200

    add new logic validation for unique number

--- EgnApp/Validation/EgnValidator.cs
+++ EgnApp/Validation/EgnValidator.cs
@@ -1,16 +1,31 @@
-﻿using EgnApp.Models;
+﻿using EgnApp.Contract;
+using EgnApp.Models;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace EgnApp.Validation
 {
     public class EgnValidator : AbstractValidator<Egn>
     {
-        public EgnValidator()
+        private readonly EgnContext _context;
+        public EgnValidator(EgnContext context)
         {
+            _context = context;
             RuleFor(guid => guid.Id).NotEmpty();
             RuleFor(name => name.Name).NotEmpty();
             RuleFor(age => age.Age).NotEmpty();
-            RuleFor(egn => egn.EgnNumber).Length(10).NotEmpty();
+            RuleFor(egn => egn.EgnNumber).NotEmpty().Length(10, 10).Must(IsUnique).WithMessage("EGN must be unique");
+
+        }
+        private bool IsUnique(int egn)
+        {
+            var model = _context.Egn.GroupBy(x => x.EgnNumber)
+               .Where(g => g.Count() > 1)
+               .Select(y => y.Key)
+               .ToList();  //judge whether parentclass has duplicate id
+            if (model == null)
+                return false;
+            else return true;
         }
     }
 }
